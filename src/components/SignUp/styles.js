c4dcb3012f7eaefe31860045a801f5762ec333de
NeptuneRjo <rjo9000@outlook.com>
2022-05-03 15:32:55 -0400

    fixed alignments

--- src/components/SignUp/styles.js
+++ src/components/SignUp/styles.js
@@ -25,7 +25,8 @@ export default makeStyles(() => ({
         width: '500px',
         borderRadius: '8px',
         boxShadow: '2px 2px 12px #00000',
-        paddingTop: '1rem'
+        paddingTop: '2rem',
+        gap: '1rem'
     },
     textField: {
         width: '350px',
@@ -34,9 +35,10 @@ export default makeStyles(() => ({
     },
     buttonContainer: {
         display: 'flex',
-        gap: '2rem',
+        gap: '1.5rem',
         justifyContent: 'flex-end',
         margin: '2rem 0',
+        marginTop: '1rem',
         width: '350px'
     }
 }))
