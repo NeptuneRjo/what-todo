7d9b96c1b80406d53ad26de601678c55d749482d
NeptuneRjo <rjo9000@outlook.com>
2022-05-05 19:03:10 -0400

    moved firestore related functions to firestore.config file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,7 +1,19 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import {
+	getFirestore,
+	collection,
+	addDoc,
+	serverTimestamp,
+	getDocs,
+	query,
+	where,
+} from 'firebase/firestore'
 
-import { getAuth } from 'firebase/auth'
+import {
+	createUserWithEmailAndPassword,
+	getAuth,
+	onAuthStateChanged,
+} from 'firebase/auth'
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
@@ -16,3 +28,45 @@ const app = initializeApp(firebaseConfig)
 
 export const db = getFirestore(app)
 export const auth = getAuth(app)
+export const colRef = collection(db, 'todos')
+
+export const updateUserIdOnAuth = () => {
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			return user.id
+		} else {
+			return null
+		}
+	})
+}
+
+export const pushNewTodoToDb = (e, todo, userId, form) => {
+	e.preventDefault()
+
+	addDoc(colRef, {
+		todo: todo,
+		completed: false,
+		createdAt: serverTimestamp(),
+		ownedBy: userId,
+	}).then(() => {
+		form.reset()
+	})
+}
+
+export const queryOwnedTodos = (array, userId) => {
+	const q = query(colRef, where('ownedBy', '==', userId))
+
+	getDocs(q).then((docs) => {
+		docs.forEach((elem) => {
+			array.push(elem.data())
+		})
+	})
+}
+
+export const signUpUser = (e, email, password, toHome) => {
+	e.preventDefault()
+
+	createUserWithEmailAndPassword(auth, email, password).then(() => {
+		toHome()
+	})
+}
