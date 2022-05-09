b8156ffb3aefe12836048f4217a249900b3ed19b
NeptuneRjo <rjo9000@outlook.com>
2022-05-09 14:31:07 -0400

    recommit

---
+++ src/components/Todos/Todos.jsx
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from 'react'
+import useStyles from './styles'
+
+import {
+	TextField,
+	Stack,
+	ButtonGroup,
+	Button,
+	Typography,
+} from '@mui/material'
+
+import { pushNewTodoToDb, colRef, updateTodo } from '../../firebase.config'
+
+import { getDocs, query, where } from 'firebase/firestore'
+
+import './style.css'
+
+const Todos = ({ userId }) => {
+	const classes = useStyles()
+
+	const [display, setDisplay] = useState('All')
+	const [todoValue, setTodoValue] = useState('')
+
+	// Exists to cause the useEffect hook to fire
+	const [rerender, setRerender] = useState(1)
+
+	const [data, setData] = useState(null)
+	const [isPending, setIsPending] = useState(true)
+	const [error, setError] = useState(null)
+
+	let userTodos = []
+	let userTodosDisplay = []
+
+	useEffect(() => {
+		const q = query(colRef, where('ownedBy', '==', userId))
+
+		getDocs(q)
+			.then((res) => {
+				return res
+			})
+			.then((res) => {
+				setIsPending(false)
+				setData(res)
+			})
+			.catch((err) => setError(err.message))
+	}, [rerender])
+
+	if (!isPending) {
+		data.forEach((elem) => {
+			userTodos.push(elem.data())
+		})
+
+		if (display === 'All') {
+			userTodosDisplay = userTodos
+		} else if (display === 'Uncompleted') {
+			for (let i = 0; i < userTodos.length; i++) {
+				if (userTodos[i].completed === false) {
+					userTodosDisplay.push(userTodos[i])
+				}
+			}
+		} else if (display === 'Completed') {
+			for (let i = 0; i < userTodos.length; i++) {
+				if (userTodos[i].completed === true) {
+					userTodosDisplay.push(userTodos[i])
+				}
+			}
+		}
+	}
+
+	// Acts as liaison for pushNewTodoToDb
+	const callPushFunction = (e) => {
+		const form = document.querySelector('#form')
+
+		pushNewTodoToDb(e, todoValue, userId, form)
+		// Causes the useEffect hook to fire again
+		setRerender(rerender + 1)
+	}
+
+	const handleTodoClick = (todo) => {
+		updateTodo(!todo.completed, todo.id)
+		setRerender(Math.random())
+	}
+
+	// Renders the todo section based on what's available
+	const todoMap = () => {
+		if (isPending) {
+			return (
+				<Typography variant='div' component='div'>
+					Loading...
+				</Typography>
+			)
+		} else if (userTodosDisplay.length === 0) {
+			return (
+				<Typography variant='div' component='div'>
+					Nothing to do...
+				</Typography>
+			)
+		}
+		return userTodosDisplay.map((todo) => (
+			<div className='todo' onClick={() => handleTodoClick(todo)}>
+				<div
+					className={`todo-item ${todo.completed ? 'completed' : 'incomplete'}`}
+				>
+					{todo.todo}
+				</div>
+			</div>
+		))
+	}
+
+	return (
+		<main className={classes.main}>
+			<Typography variant='div' component='div' className={classes.header}>
+				{display}
+				<br />
+				Todos
+			</Typography>
+			<form className={classes.inputContainer} id='form'>
+				<TextField
+					required
+					id='outlined-required'
+					label='What Todo'
+					className={classes.input}
+					name='todo'
+					onChange={(e) => setTodoValue(e.target.value)}
+				/>
+				<Button
+					variant='outlined'
+					type='submit'
+					onClick={(e) => callPushFunction(e)}
+				>
+					Submit
+				</Button>
+			</form>
+
+			<Stack direction='column' spacing={2} className={classes.stack}>
+				{todoMap()}
+			</Stack>
+			<ButtonGroup
+				variant='outlined'
+				aria-label='outlined primary button group'
+				className={classes.buttonGroup}
+			>
+				<Button onClick={() => setDisplay('All')}>All</Button>
+				<Button onClick={() => setDisplay('Uncompleted')}>Uncompleted</Button>
+				<Button onClick={() => setDisplay('Completed')}>Completed</Button>
+			</ButtonGroup>
+		</main>
+	)
+}
+
+export default Todos
---
+++ src/components/Todos/styles.js
@@ -0,0 +1,35 @@
+import { makeStyles } from '@mui/styles'
+
+export default makeStyles(() => ({
+	main: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexDirection: 'column',
+		height: '100vh',
+		background: '#fff',
+	},
+	input: {
+		color: '#fff',
+		width: '300px',
+	},
+	stack: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: '450px',
+		overflowY: 'scroll',
+		width: '300px',
+		margin: '1rem 0',
+		border: 'solid 1px #303030',
+	},
+	buttonGroup: {
+		width: '300px',
+	},
+	header: {
+		textAlign: 'center',
+		padding: '1rem 0',
+		fontSize: '1.1em',
+	},
+}))
