10fb392b97d2554e45e261d1efd2b0c78c6d0557
NeptuneRjo <rjo9000@outlook.com>
2022-05-09 19:44:41 -0400

    todo list now updates when user logs in

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,72 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useStyles from './styles'
 
-import { 
-    Button, TextField, Typography,
-} from '@mui/material'
+import { Button, TextField, Typography } from '@mui/material'
 
-import {
-    signInWithEmailAndPassword
-} from 'firebase/auth'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 
-const SignIn = ({ auth }) => {
+import { useNavigate } from 'react-router-dom'
 
-    const classes = useStyles();
-    const signInForm = document.querySelector('#signin')
+import { auth } from '../../firebase.config'
 
-    const signInUser = (e) => {
-        e.preventDefault()
+const SignIn = ({ setUserId }) => {
+	const classes = useStyles()
+	let navigate = useNavigate()
 
-        const email = signInForm.email.value
-        const password = signInForm.password.value
+	const [emailValue, setEmailValue] = useState('')
+	const [passwordValue, setPasswordValue] = useState('')
 
-        signInWithEmailAndPassword(auth, email, password)
-    }
+	const signInUser = (e) => {
+		e.preventDefault()
 
+		signInWithEmailAndPassword(auth, emailValue, passwordValue).then((cred) => {
+			setUserId(null)
+		})
+		navigate('/')
+	}
 
-    return (
-        <main className={classes.main}>
-            <Typography 
-                variant='body' 
-                component='div' 
-                className={classes.header}
-            >
-                Sign In:
-            </Typography>
-            <form
-                id='signin'
-                className={classes.content}
-            >
-                <TextField
-                    required
-                    id="outlined-required  email"
-                    label="Email address"
-                    type='email'
-                    className={classes.textField}
-                    name='email'
-                />
-                <TextField
-                    id="outlined-password-input password"
-                    label="Password"
-                    type="password"
-                    name='password'
-                    className={classes.textField}
-                    required
-                    inputProps={{ minLength: 6 }}
-                />
-                <div className={classes.buttonContainer}>
-                    <Button 
-                        variant='contained' 
-                        type='submit' 
-                        onClick={(e) => signInUser(e.target)}
-                    >
-                        Sign In
-                    </Button>
-                    <Button variant='outlined'>Cancel</Button>
-                </div>
-            </form>
-        </main>
-    )
+	return (
+		<main className={classes.main}>
+			<Typography variant='body' component='div' className={classes.header}>
+				Sign In:
+			</Typography>
+			<form id='signin' className={classes.content}>
+				<TextField
+					required
+					id='outlined-required  email'
+					label='Email address'
+					type='email'
+					className={classes.textField}
+					name='email'
+					onChange={(e) => setEmailValue(e.target.value)}
+				/>
+				<TextField
+					id='outlined-password-input password'
+					label='Password'
+					type='password'
+					name='password'
+					className={classes.textField}
+					required
+					inputProps={{ minLength: 6 }}
+					onChange={(e) => setPasswordValue(e.target.value)}
+				/>
+				<div className={classes.buttonContainer}>
+					<Button
+						variant='contained'
+						type='submit'
+						onClick={(e) => signInUser(e)}
+					>
+						Sign In
+					</Button>
+					<Button variant='outlined' onClick={() => navigate('/')}>
+						Cancel
+					</Button>
+				</div>
+			</form>
+		</main>
+	)
 }
 
 export default SignIn
