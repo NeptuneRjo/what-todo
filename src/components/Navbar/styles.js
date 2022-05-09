1336c72d3f87c8d1b951f6c200833ae0ec60270c
NeptuneRjo <rjo9000@outlook.com>
2022-05-09 18:45:58 -0400

    added and styled navbar

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@mui/styles'
+
+export default makeStyles(() => ({
+	toolbar: {
+		display: 'flex',
+		justifyContent: 'space-between',
+	},
+	email: {
+		height: '24.5px',
+	},
+	link: {
+		textDecoration: 'none',
+		color: 'inherit',
+	},
+}))
