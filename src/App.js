a9605591dc481e74bae403751ef04622521c4bf2
NeptuneRjo <rjo9000@outlook.com>
2022-05-09 14:25:22 -0400

    added auth listener to update id whenever someone logs in / signs up

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-import { initializeApp } from 'firebase/app'
-import {
-    getFirestore, collection, addDoc,
-    serverTimestamp
-} from 'firebase/firestore'
+import { SignUpForm, SignIn, Todos } from './components'
 
-import { 
-    getAuth,
-    createUserWithEmailAndPassword,
-    signOut, signInWithEmailAndPassword
-} from 'firebase/auth'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
-import { SignUpForm, SignIn } from './components'
+import { auth } from './firebase.config'
 
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
 
 const App = () => {
-    
-    const firebaseConfig = {
-        apiKey: process.env.REACT_APP_API_KEY,
-        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_MSSGNG_ID,
-        appId: process.env.REACT_APP_APP_ID
-    };
+	const [userId, setUserId] = useState(null)
 
-    const app = initializeApp(firebaseConfig);
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			const uid = user.uid
+			setUserId(uid)
+		} else {
+			setUserId('default')
+		}
+	})
 
-    // Initialized Firebase services
-    const db = getFirestore();
-    const auth = getAuth();
-
-    // Database reference (todos)
-    const colRef = collection(db, 'todos')
-
-    return (
-        <Router>
-            <Routes>
-                <Route path='/sign-up' element={<SignUpForm auth={auth} />}/>
-                <Route path='/sign-in' element={<SignIn auth={auth} />}/>
-            </Routes>
-        </Router>
-    )
+	if (userId !== null) {
+		return (
+			<HashRouter>
+				<Routes>
+					<Route
+						path='/sign-up'
+						element={<SignUpForm setUserId={setUserId} />}
+					/>
+					<Route path='/sign-in' element={<SignIn />} />
+					<Route exact path='/' element={<Todos userId={userId} />} />
+				</Routes>
+			</HashRouter>
+		)
+	}
 }
 
 export default App
